e827482391959514dd4c0f1947a738418ea6e8c5
Adrian Siara <adrian.siara.praca@gmail.com>
2023-05-04 16:32:21 +0200

    modified:   index.php

--- index.php
+++ index.php
@@ -50,7 +50,7 @@
     <div class="modal-content">
       <div class="modal-header">
         <h5 class="modal-title" id="exampleModalLabel">ADD STUDENTS</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+        <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
